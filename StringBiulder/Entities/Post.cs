29c6b46e1c86c0a98dd235da304aa296c9c07077
Flávio Pinheiro <fpssouzza@gmail.com>
2019-07-19 19:51:09 -0300

    Usando Composição e StringBuilder

---
+++ StringBiulder/Entities/Post.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringBiulder.Entities
+{
+    class Post
+    {
+        // Declaração dos argumentos
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+        // Pelo fato dos comentários poderem muitos 'Commments' foi declarado tipo List e intânciado
+        // para forçar sua inicialização.
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+        // Declaração do construtor padrão
+        public Post()
+        {
+
+        }
+        // Declaração do contrutor personalizado
+        // OBS.: quando se tem um relacionamento para muitos (neste caso um post e muitos comentários)
+        // este atributo não é declarado no contrutor.
+        public Post(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+        //----------------------------------------------------------------------------------------
+        // Obs.: embora os dois metodos abaixo não estejam especificados no desenho de diagrama
+        // eles devem ser declarados (somente em casos especificos eles são evitados)
+        //----------------------------------------------------------------------------------------
+
+        // Declaração do metodo para adição de comentários
+        public void AddComment(Comment comment)
+        {
+            Comments.Add(comment);
+        }
+        // Declaração do metodo para remoção de comentários
+        public void RemoveComment(Comment comment)
+        {
+            Comments.Remove(comment);
+        }
+        // Usando a classe StringBuilder para montagem mais eficiente para
+        // montar saida de texto para o usuário.
+        // Pertence ao Name Space System.Text
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(Title);
+            sb.Append(Likes);
+            sb.Append(" Likes - ");
+            sb.AppendLine(Moment.ToString("dd/mm/yyyy HH:mm:ss"));
+            sb.AppendLine(Content);
+            sb.AppendLine("Comments: ");
+
+            // Percorre a lista de comentários e passa para o objeto 'sb' instanciado com StringBuider
+            // que será chamado pelo programa padrão.
+            foreach (Comment c in Comments)
+            {
+                sb.AppendLine(c.Text);
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ StringBiulder/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using StringBiulder.Entities;
+
+namespace StringBiulder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Comment c1 = new Comment("Tenha uma boa viagem");
+            Comment c2 = new Comment("Ual isto é muito legal!");
+
+            Post p1 = new Post(DateTime.Parse("21/06/2018 13:05:44"), "Viajando para Nova Zelandia",
+                "Estou indo visitar esse maravilhoso pais!", 12);
+
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+
+            Comment c3 = new Comment("Boa noite");
+            Comment c4 = new Comment("Que a força esteja com você");
+
+            Post p2 = new Post(DateTime.Parse("28/07/2018 23:14:19"), "Boa noite pessoal",
+                "Vejo vocês amanhã.", 5);
+            p2.AddComment(c3);
+            p2.AddComment(c4);
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+
+
+        }
+    }
+}
